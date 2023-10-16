f29d68d2a5abd99df2cc80ccfea4a5c85b77b420
BIZON Nolan <Nolan.Bizon@bordeaux-inp.fr>
2023-10-16 13:11:42 +0200

    Add the installation file for linux

---
+++ installation_linux/IA_SeReOs_install_2.sh
@@ -0,0 +1,23 @@
+pip install adabelief-pytorch
+pip install unet
+pip install streamlit-server-state
+conda install -c nvidia cuda-python
+conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
+pip install Pillow
+pip install pandas
+pip install opencv-python
+pip install SimpleITK
+pip install h5py
+pip install PyYAML
+pip install numpy
+pip install pyvista
+pip install plotly
+pip install matplotlib
+pip install torchvision
+pip install tensorflow
+pip install tensorboard
+pip install tqdm
+pip install scikit-learn
+pip install pytorch-wavelets
+pip install albumentations --user
+pip install vtk
